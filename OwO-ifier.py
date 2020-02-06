2e7c4cd5aa55aaa21200f4afcb1f5de8c87186d6
meekylemondal123 <56230994+meekylemondal123@users.noreply.github.com>
2020-02-05 21:50:45 -0500

    Finishing up the OwO-ifier
    This will now only replace 'v's only if they appear at the beginning of the word. Further changes in terms of proper owo-ification will be added in later commits

--- OwO-ifier.py
+++ OwO-ifier.py
@@ -22,41 +22,56 @@ def main():
 
     print (newString)
     
-def replace(replaceLetter, index, fullString):
-    # This method will replace the letter at the current index with the appropriate replacement char sequence
-    return fullString[0:index] + replaceLetter + fullString[index:]    
+
 
     
 def locate(letter, index, fullString):
     newLetter = ""
+    changes = False
     
     # This method locates the index of all the letters 
-    if letter.lower() == "r" or letter.lower() == "v" or letter.lower() == "l" or letter.lower() == "m":
+    if letter.lower() == "r" or letter.lower() == "l" or letter.lower() == "m":
         # Replacing the letter with so and so
-        if letter.isUpper() == True:
+        if letter.isupper() == True:
             newLetter = "W"
+            changes = True
         else:
             newLetter = "w"
+            changes = True
 
     elif letter == ".":
         value = randint(1, 2)
 
         if (value == 1):
-            newLetter = "uwu"
+            newLetter = " uwu. "
+            changes = True
         else:
-            newLetter = "owo"
-
+            newLetter = " owo. "
+            changes = True
             
+    elif letter == "v":
+            if index == 0:
+                newLetter = "w"
+            else:
+                if fullString[index - 1] == " ":
+                    newLetter = "w"
+
+                
     if index < len(fullString) - 1 and newLetter == "":
         # This is to avoid the possibility of detectinf and e and then checking for an a outside of the string index
         if letter == "e":
             if fullString[index + 1] == "a":
                 newLetter = "ee"
+                changes = True
         elif letter == "s":
             if fullString[index + 1] == "e":
                 newLetter = "z"
+                changes = True
 
 
-    return replace(newLetter, index, fullString)
+    if changes:
+        return newLetter
+    else:
+        return letter
 
 main()
